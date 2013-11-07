36aacd613294b8a8a1e8b7ce8de6fcf877abfb70
Remco Tukker <helium_2@hotmail.com>
2013-11-07 16:59:30 +0100

    minimal version of agent functionality

--- agentBase.js
+++ agentBase.js
@@ -1,19 +1,76 @@
 
+
+// functions for sending stuff back to the main thread
+
+invokeMethod = function(methodName, params, stateKeys, time) {
+
+	console.log("sending invokeMethod event");
+	console.log('invokeMethod ' + time +" "+ methodName+" " + JSON.stringify(params)+" " + JSON.stringify(stateKeys));
+	//parse this nicely in the events that the main thread is listening for
+	thread.emit('invokeMethod', time, methodName, JSON.stringify(params), JSON.stringify(stateKeys));
+}
+	
+send = function(destination, data) {
+		
+	thread.emit('sendEveMessage', destination, JSON.stringify(data));
+}
+
+store = function(key, value) {
+		
+	thread.emit('storeData', key, JSON.stringify(value));
+}
+
+
 function agentBase() { //encapsulates all basic functionality that the agent can use
 	
 
 
-	this.invokeMethod = function(methodName, args, stateKeys, time) {
-		//parse this nicely in the separate events that the main thread is listening for
-	}
 	
-	this.
+
+	//thread.on(...)  //we're not sending any events from the main thread to here, so we dont have to listen...
+
+	// obligatory methods 
+	// TODO: help users with taking care of these
+	this.getMethods = function() {  ///TODO: preferably add some sugar functions to allow functions with parameter lists instead of a parameter object
+
+	}
+
+	this.getId = function() {
+
+	}
+
+	this.getUrls = function() {
+
+	}
+
 
 
 }
 
 
-on()
+// end user generated code example
 
 
-emit
+var myAgent = new agentBase(); //TODO: how does the caller in the main thread know what this object is called exactly? (right now it doesnt and thats nasty)
+								//also, coupling the name of the file to the agent object that is defined here is not so nice (filename should be irrelevant)
+//if we do it this way, myAgent cannot really be extracted as far as I could tell.. need to make a separate constructor function and set agentBase as prototype?
+
+myAgent.myFunction = function(params) {
+	
+	var a = parseInt(params.a);
+	var b = parseInt(params.b);
+	var result = a + b;
+	store("result", a + b);
+
+	console.log("myfunction called with " + a + " " + b);
+
+	send("bla", "bla");
+	
+	console.log("2");
+
+	invokeMethod("myAgent.myFunction", {a:1}, {b:"result"}, 1);
+	
+	console.log("3");
+	//return a + b;
+}
+
--- eve.js
+++ eve.js
@@ -2,7 +2,7 @@
 var http = require('http'),
     url = require('url'),
     //couch_client = require('couch-client'),
-	Threads = require('webworker-threads'); //webworker threads is better than TAGG ´cause this one lets you do importscript
+	Threads = require('webworker-threads'); //webworker threads is better than TAGG ´cause this one lets you do importscript (does it in threads? prolly not -> TODO)
     //ManagerAgent = require('./agent/ManagerAgent.js'),
 	//Threads = require('threads_a_gogo'); 
 
@@ -31,61 +31,117 @@ function dataStore() {
 }
 
 //object representing main-thread side of agent  
-function agent(filename, url, threads)
+function Agent(filename, url, threads)
 {
 	//constructor
 	var agentData = new dataStore();
 
 	//save the init parameter for some introspection
-	agentData.save("address", url); 
-	agentData.save("threads", threads);
-	agentData.save("filename", filename);
+	agentData.save("address", JSON.stringify(url)); 
+	agentData.save("threads", JSON.stringify(threads));
+	agentData.save("filename", JSON.stringify(filename));
 
 	//have a thread pool for every agent (because threads-a-gogo requires file to be loaded beforehand; loading code into threads on the fly is likely not very performant)
 	var pool = Threads.createPool(threads); 
 	pool.load(__dirname + filename);   //load the file in the threads so that they are ready for execution
 
+	//function for running a function in a thread
+	this.invokeMethod = function(methodName, params) {
+
+		console.log("invoking method " + methodName + '(' + params + ')');	
+
+		pool.any.eval(methodName + '(' + params + ')'); 
+		//  TODO think about security implicactions of eval'ing external messages on the thread.. can we do this better? always send it to a dispatcher function?
+		///TODO hrm... perhaps make it so that there should always be a return value? for the response of the agent I mean...
+		// then we should add a callback for once the agent replies to us
+	}
+
 	// ********* Handlers for events that can be generated in the threads ***************
-	// invoke one of your own methods
-	pool.on("invokeMethod", function(nextFunction, args) {
-		invokeMethod(nextFunction, args);
-	});
 
-	// event listener to handle state requests
-	pool.on("invokeMethodWithState", function(nextFunction, args, statekeys) {
-		//recall all state that the new function call requires		
-		//agentData.recall(key)
-		
-		//combine args and statekeys for the next function call (how to do this?)
-
-		//invoke the method		
-		invokeMethod(nextFunction, args);
-
+	// event listener to store something in the state
+	pool.on("storeData", function(key, value) {
+		agentData.save(key, JSON.parse(value));  //would be nicer to store all data stringified, but we have to parse and stringify it again before sending anyway
 	});
 
 	// event listener to send messages
 	pool.on("sendEveMessage", function(destination, data)  {
 		//create and send the json rpc message (see Jos' work.. hrm no use, wasnt implemented yet)
 
+		console.log("got sendEveMessage event");	
+
 	});
 
-	pool.on("scheduleMethod", function(time, nextFunction, args) {
+	var that = this; //there's prettier ways to do this.. doesnt matter... used later on in scheduling new tasks		
+
+	// event listener for invoking other agent methods
+	pool.on("invokeMethod", function(time, functionName, strParams, strStateKeys) {
+		//recall all state that the new function call requires		
+		//and add the recalled state to the params object
+		//TODO: check wheter this is working as intended
+		params = JSON.parse(strParams);
+		stateKeys = JSON.parse(strStateKeys);
+
+		console.log("got invokeMethod event");	
+		console.log(time + " " + functionName+ " " + params + " " + stateKeys );
+
+		for (key in stateKeys) {
+			console.log(key);
+			params[key] = agentData.recall(stateKeys[key]);
+		}
+
+		console.log("got invokeMethod event");	
+		console.log(time + " " + functionName+ " " + JSON.stringify(params) + " " + JSON.stringify(stateKeys) );
+
+		if (time == 0) { //check how this statement evaluates in edge cases (undefined etc)
+			process.nextTick(function() { return that.invokeMethod(fucntionName, JSON.stringify(params)); });
+		} else { 
+			setTimeout(function() { return that.invokeMethod(functionName, JSON.stringify(params)); }, time);
+		}
+
+	});
+
+
+/* TODO: make seperate event callbacks for the schedule and invoke events to minimize the number of checks and operations per callback, as follows
+
+	// invoke one of your own methods
+	pool.on("invokeMethod", function(nextFunction, params) {
+		invokeMethod(nextFunction, params);
+	});
+
+	// event listener to handle state requests
+	pool.on("invokeMethodWithState", function(nextFunction, params, statekeys) {
+		//recall all state that the new function call requires		
+		//and add the recalled state to the params object
+		//TODO: check wheter this is working as intended
+		for (key in statekeys) {
+			params.key = agentData.recall(key);
+		}
+
+		//invoke the method		
+		invokeMethod(nextFunction, params);
+	});
+
+	pool.on("scheduleMethod", function(time, nextFunction, params) {
 		var that = this; //there's prettier ways to do this.. doesnt matter...		
-		setTimeout(function(){ return that.invokeMethod(nextFunction, args); }, time); //TODO: fix args ... probably use an object to be able to use "named parameters"
+		setTimeout(function(){ return that.invokeMethod(nextFunction, params); }, time);
 	});
 
-	pool.on("scheduleMethodWithState", function(time, nextFunction, args, statekeys) {
-
-
+	pool.on("scheduleMethodWithState", function(time, nextFunction, params, statekeys) {
+		//add recalled values to params
+		for (key in statekeys) {
+			params.key = agentData.recall(key);
+		}
+		//and schedule the function
+		var that = this; //there's prettier ways to do this.. doesnt matter...		
+		setTimeout(function(){ return that.invokeMethod(nextFunction, params); }, time);
 	});
+*/
 
-	this.invokeMethod = function(methodName, args) {
-		pool.any.eval(methodName + '(' + args + ')'); 
-	}
+	
 
 	//TODO: agent self-management:
 	//create a way to temporarily block incoming messages and answer them with an "out of office" reply, such that we can re-initialize threadpool to:
-	// * change the number of threads for this agent (after checking pendingJobs() somewhere to see if the thread pool is the right size)
+	// * change the number of threads for this agent (after checking pendingJobs() a couple of times to see if the thread pool is the right size)
 	// * load a different js file in the threads, for effectively transfering your address to a new agent and for allowing modifying code, eg to inject a new function in an agent
 
 	//TODO: stash agent away Jos-style in case it is not used for a while
@@ -103,12 +159,17 @@ function agent(filename, url, threads)
 var agentList = [];
 var urlToAgentMapping = new Object();
 
-eve.add = function(filename) {
+eve.add = function(filename, options) {
 
+	//TODO use options object for the following:
 	//TODO get an initial decision on how many threads to use (preferably from user?)
 	//TODO get a url to use for this agent, preferably check whether it wasnt in use yet..
+	//TODO  !!!!! we need stack traces from dying agents!!!! best solution: hack this into TAGG. Alternative: add a debug option which lets you run the agent in the main thread
+ 	//                  eg in a separate node instance from the rest of the agents, or in a child process of node
+
 	var threads = 5;	
 	var url = "bla"; 
+
 	urlToAgentMapping[url] = agentList.length;	
 	agentList.push(new Agent(filename, url, threads) );
 
@@ -125,10 +186,10 @@ eve.handleRequest = function (agentType, agentId, request, callback) {
 	var targetAgentURL = agentType + "/" + agentId; //do we need this agentID?
 	//do some sanity checks around here
 	var targetAgentNr = urlToAgentMapping[targetAgentURL];
-	agentList[targetAgentNr].invokeMethod(req.method, req.params);
+	agentList[targetAgentNr].invokeMethod(req.method, JSON.stringify(req.params));
 
 
-	//placeholder for some intelligent agent-level, json rpc id based list thats waiting for responses
+	//placeholder for some intelligent agent-level, json rpc id based list thats waiting for responses TODO
 	var response = {
        "id": id,
         "result": null,
@@ -166,7 +227,7 @@ eve.listen = function (port, host) {
 
             eve.handleRequest(type, id, data, function(response) { 
 				//hrm, is this really necessary here? can we not decide on our own whether or when to send something back?
-				//we then have to keep the callback around until the agent decide to send something back...
+				//if we do it like this, we to keep the callback around until the agent decide to send something back... and at some point just give up waiting... or not?
                 console.log(response);
                 res.writeHead(200, {'Content-Type': 'application/json'});
                 res.end(response);
@@ -174,6 +235,16 @@ eve.listen = function (port, host) {
         });
     }).listen(port, host);
 };
+
+
+//// test lines here
+
+eve.add("/agentBase.js");
+//agentList[0].invokeMethod("myAgent.myFunction", JSON.stringify({a:4, b:4}));
+agentList[0].invokeMethod("myAgent.myFunction", JSON.stringify({a:4, b:4}));
+//agentList[0].invokeMethod("myAdd", JSON.stringify({a:4, b:4}));
+
+
 
 /**
  * nodejs exports
